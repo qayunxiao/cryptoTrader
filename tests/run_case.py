de06b62ad9c35070ba523d52a549a566ca8d3c01
alvinsss <6449694@qq.com>
2024-01-02 15:40:41 +0800

    ALL

--- tests/run_case.py
+++ tests/run_case.py
@@ -7,6 +7,7 @@
 import datetime
 import os
 import sys
+import time
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 utils = os.path.join(BASE_DIR,'utils')
@@ -19,18 +20,19 @@ from process_data.symbol_price_history import get_history_data
 
 
 def run_get_current_price():
-    symbol_price =  ['BTC', 'ETH']
-    symbol_ls =  ['BTC', 'ETH', 'DOT', 'FIL', 'LINK', 'LINK', 'LTC', 'NEAR', 'SOL', 'DASH', 'SAND', 'AR', 'APE',
-                 'SNX', 'RAY', 'MINA', 'ICP', 'DYDX', 'NEO', 'MOVR', 'ADA', 'RNDR', 'STX', 'MAGIC', 'GLMR', 'C98', 'ATOM', 'MAGIC']
+    # symbol_price =  ['BTC', 'ETH']
+    symbol_price =  ['BTC', 'ETH', 'DOT', 'FIL', 'LINK' , 'LTC', 'NEAR', 'SOL', 'DASH', 'SAND', 'AR', 'APE',
+                 'SNX', 'RAY', 'MINA', 'ICP', 'DYDX', 'NEO', 'MOVR', 'ADA', 'RNDR', 'STX', 'MAGIC', 'GLMR', 'C98', 'ATOM']
     get_data_price(symbol_price,str(datetime.datetime.now().date()))
 
 
 def run_get_history_data():
-    symbol_price_history =  ['BTC', 'DOT', 'ETH', 'ICP', 'LINK', 'UNI']
+    symbol_price_history = ['BTC','ETH','DOT','FIL','LINK','LTC','ICP','SNX','LINK','UNI']
     get_history_data(symbol_price_history)
 
 
 
 if __name__ == '__main__':
     # run_get_current_price()
-    run_get_current_price()
+    # time.sleep(2)
+    run_get_history_data()
