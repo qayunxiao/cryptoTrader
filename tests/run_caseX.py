a4934cb33ed935ed7c2d9324fad86a4e2d1a08b7
alvinsss <6449694@qq.com>
2024-01-03 13:24:14 +0800

    x

--- tests/run_caseX.py
+++ tests/run_caseX.py
@@ -17,35 +17,42 @@ print("utils is :{}".format(utils))
 
 from utils.crypto.handle_getKline import get_data_Xprice
 
+# symbol_price =  ['BTC', 'ETH']
+symbol_price = ['BTC', 'ETH', 'DOT', 'FIL', 'LINK', 'LTC', 'NEAR', 'SOL', 'DASH', 'SAND', 'AR', 'APE',
+                'SNX', 'RAY', 'MINA', 'ICP', 'DYDX', 'NEO', 'MOVR', 'ADA', 'RNDR', 'STX', 'MAGIC', 'GLMR',
+                'C98', 'ATOM', 'BADGER', 'CFX']
 
-def run_get_current_Xprice():
+
+def run_get_current_Xprice2():
     # symbol_price =  ['BTC', 'ETH']
-    symbol_price = ['BTC', 'ETH', 'DOT', 'FIL', 'LINK', 'LTC', 'NEAR', 'SOL', 'DASH', 'SAND', 'AR', 'APE',
-                    'SNX', 'RAY', 'MINA', 'ICP', 'DYDX', 'NEO', 'MOVR', 'ADA', 'RNDR', 'STX', 'MAGIC', 'GLMR', 'C98',
-                    'ATOM']
     X = 3
     get_data_Xprice(symbol_price, str(datetime.datetime.now().date()), X)
 
 
-def run_get_current_Xprice5():
+def run_get_current_Xprice3():
     # symbol_price =  ['BTC', 'ETH']
-    symbol_price = ['BTC', 'ETH', 'DOT', 'FIL', 'LINK', 'LTC', 'NEAR', 'SOL', 'DASH', 'SAND', 'AR', 'APE',
-                    'SNX', 'RAY', 'MINA', 'ICP', 'DYDX', 'NEO', 'MOVR', 'ADA', 'RNDR', 'STX', 'MAGIC', 'GLMR', 'C98',
-                    'ATOM']
+    X = 3
+    get_data_Xprice(symbol_price, str(datetime.datetime.now().date()), X)
+
+
+def run_get_current_Xprice4():
+    X = 4
+    get_data_Xprice(symbol_price, str(datetime.datetime.now().date()), X)
+
+
+def run_get_current_Xprice5():
     X = 5
     get_data_Xprice(symbol_price, str(datetime.datetime.now().date()), X)
 
 
 def run_get_current_Xprice10():
-    # symbol_price =  ['BTC', 'ETH']
-    symbol_price = ['BTC', 'ETH', 'DOT', 'FIL', 'LINK', 'LTC', 'NEAR', 'SOL', 'DASH', 'SAND', 'AR', 'APE',
-                    'SNX', 'RAY', 'MINA', 'ICP', 'DYDX', 'NEO', 'MOVR', 'ADA', 'RNDR', 'STX', 'MAGIC', 'GLMR', 'C98',
-                    'ATOM']
     X = 10
     get_data_Xprice(symbol_price, str(datetime.datetime.now().date()), X)
 
 
 if __name__ == '__main__':
-    run_get_current_Xprice()
+    run_get_current_Xprice2()
+    run_get_current_Xprice3()
+    run_get_current_Xprice4()
     run_get_current_Xprice5()
     # run_get_current_Xprice10()
