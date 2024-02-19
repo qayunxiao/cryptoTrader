d8ec518d445106d5cde21941464068c1231c3e08
alvinsss <6449694@qq.com>
2024-02-19 09:38:01 +0800

    倍数值不对

--- tests/run_caseX.py
+++ tests/run_caseX.py
@@ -21,25 +21,47 @@ from utils.crypto.handle_getKline import get_data_Xprice
 symbol_price = ['BTC', 'ETH', 'DOT', 'LINK', 'FIL', 'OP', 'LTC', 'SOL', 'ENS', 'NEAR', 'PEOPLE', 'SNX',
                 'DYDX', 'STX', 'DASH', 'LDO', 'SAND', 'APE', 'MATIC', 'DOGE', 'ICP', 'APT', 'ADA', 'MAGIC',
                 'MINA', 'MANTA', 'ATOM', 'PYTH', 'BLUR', 'ALT', 'TIA', 'SEI']
+
+
 def run_get_current_Xprice2():
     X = 2
     get_data_Xprice(symbol_price, str(datetime.datetime.now().date()), X)
+
 
 def run_get_current_Xprice3():
     X = 3
     get_data_Xprice(symbol_price, str(datetime.datetime.now().date()), X)
 
+
 def run_get_current_Xprice4():
     X = 4
     get_data_Xprice(symbol_price, str(datetime.datetime.now().date()), X)
+
 
 def run_get_current_Xprice5():
     X = 5
     get_data_Xprice(symbol_price, str(datetime.datetime.now().date()), X)
 
+def run_get_current_Xprice6():
+    X = 6
+    get_data_Xprice(symbol_price, str(datetime.datetime.now().date()), X)
+
+def run_get_current_Xprice7():
+    X = 7
+    get_data_Xprice(symbol_price, str(datetime.datetime.now().date()), X)
+
+def run_get_current_Xprice8():
+    X = 8
+    get_data_Xprice(symbol_price, str(datetime.datetime.now().date()), X)
+
+def run_get_current_Xprice9():
+    X = 9
+    get_data_Xprice(symbol_price, str(datetime.datetime.now().date()), X)
+
 def run_get_current_Xprice10():
     X = 10
     get_data_Xprice(symbol_price, str(datetime.datetime.now().date()), X)
+
 
 if __name__ == '__main__':
     run_get_current_Xprice2()
