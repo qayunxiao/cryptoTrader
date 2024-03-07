5a89ef30f1a1a90f1c3bf8ea7db256690db601fb
alvinsss <6449694@qq.com>
2024-03-07 09:30:44 +0800

    ALL

--- process_data/symbol_price_history.py
+++ process_data/symbol_price_history.py
@@ -92,7 +92,7 @@ def get_history_data(symbol_list):
         send_ding_msgs("综合逃顶判断:当前恐慌指数:{},当前ahr999值:{} 考虑分批减仓".format(fear_value[0], resapi_ahr_data_today['ahr999']), myself='alvin')
 
     # 逃顶判断
-    if fear_value[0] > 92 or resapi_ahr_data_today["ahr999"] > 1.53:
+    if fear_value[0] > 93 or resapi_ahr_data_today["ahr999"] > 1.7:
         # log.error("恐慌和ahr指标综合,当前恐慌指数:{},当前ahr999值:{} 考虑分批减仓".format(fear_value[0], resapi_ahr_data_today['ahr999']))
         # send_ding_msgs("综合逃顶判断:当前恐慌指数:{},当前ahr999值:{} 考虑分批减仓".format(fear_value[0], resapi_ahr_data_today['ahr999']))
         send_ding_msgs("综合逃顶判断-中长线:当前恐慌指数:{},当前ahr999值:{} 考虑分批减仓".format(fear_value[0], resapi_ahr_data_today['ahr999']), myself='alvin')
