e5f2827d0025dffdd6f520b68e41a62cddffd001
alvin <6449694@qq.com>
2024-07-24 11:28:42 +0800

    ALL

---
+++ process_data/get_eth.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 无限循环
+while true; do
+    # 使用 curl 和 jq 获取ETHUSDT的数据
+    eth_value=$(curl -s 'https://api.binance.com/api/v3/depth?symbol=ETHUSDT&limit=1' | jq -r '.asks[0][0]')
+
+    # 检查curl命令是否成功
+    if [ $? -eq 0 ]; then
+        # 使用printf格式化输出，保留两位小数
+        formatted_value=$(printf "%.2f" "$eth_value")
+        echo -ne "\033]0;E: $formatted_value\007"
+    else
+        echo "Failed to fetch data"
+    fi
+
+    # 每10秒钟执行一次
+    sleep 10
+done
