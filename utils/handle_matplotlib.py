f5db9dead5851d5aaf8e6cb0fa60d740de58a882
wanghailin <wanghailin@letsflytech.com>
2024-11-22 18:20:37 +0800

    matplotlib去掉

--- utils/handle_matplotlib.py
+++ utils/handle_matplotlib.py
@@ -3,10 +3,10 @@
 # @Author  : alvin
 # @File    : handle_matplotlib.py
 # @Software: PyCharm
-import matplotlib.pyplot as plt
-import numpy as np
-np.random.seed(1234)
-N=1000
-randnorm = np.random.normal(size=N)
-counts, bins, path = plt.plot(randnorm, bins = np.sqrt(N), normed = True, color = 'blue')
-plt.show()
+# import matplotlib.pyplot as plt
+# import numpy as np
+# np.random.seed(1234)
+# N=1000
+# randnorm = np.random.normal(size=N)
+# counts, bins, path = plt.plot(randnorm, bins = np.sqrt(N), normed = True, color = 'blue')
+# plt.show()
