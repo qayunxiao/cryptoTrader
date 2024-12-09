a63e340077def2e167aead3d517982d6dc859f08
wanghailin <wanghailin@letsflytech.com>
2024-12-09 09:25:45 +0800

    增加NEAR，2024-12新进场的LDO,ADA记录

--- tests/run_case_all.py
+++ tests/run_case_all.py
@@ -49,6 +49,16 @@ class run_priceX():
         self.costPricecountxiaohao1 = [{'BTC':[24920,0.5]},{'BTC':[25800,0.9]},{'BTC':[16000,0.15]},{'BTC':[15800,0.6]},{'BTC':[16800,0.76]},{'BTC':[21999,0.5]},{'BTC':[21700,0.4]},{'BTC':[21550,0.4]},{'BTC':[23450,0.4]},{'BTC':[23618,0.4]},{'BTC':[21200,0.5]},{'BTC':[20370,0.16655]},{'BTC':[21518,0.1]},{'BTC':[19955,0.3152]},{'ETH':[1484,10]},{'ETH':[1588,10]},{'ETH':[1975,2.7]},{'ETH':[2477,6]},{'ETH':[2500,4]},{'UNI':[8.66,1000]},{'FIL':[5.37,1900]},{'SOL':[90.73,92.2]},{'LINK':[6,500]},{'LINK':[15,50]},{'NEAR':[1.16,1158]},{'OP':[2.8,3350]},{'PYTH':[0.47,17732]} ,{'STX':[2.07,5971]},{'ENS':[16.84,365]},{'SNX':[3.36,1999]},{'DYDX':[2.84,1500]},{'GALA':[0.0275,72727]},{'PEOPLE':[0.0285,100000]},{'APE':[1.62,1000]},{'APE':[1.28,1075]},{'LTC':[85,30]},{'SUI':[1.72,872]},{'SUI':[1.48,100]},{'DASH':[30,76]},{'DOGE':[0.0916,10917]},{'SAND':[0.3095,646]},{'SAND':[0.7,3000]},{'SAND':[0.493,1334]},{'DASH':[30,75]},{'DOGE':[0.0916,10922]},{'DOGE':[0.14,1110]},{'ALT':[0.34,7042]}  ,{'ADA':[0.26,904]}]
         self.costPricecountxiaohao2 = [{'BNB': [306, 7]}, {'BNB': [280, 10]}, {'BNB': [366, 8]}, {'BNB': [377, 6.3]},{'ARKM': [2.5, 5294]},{'TIA': [12.47, 719]},{'FET': [2.46, 2283]}  , {'WIF': [3.69, 1047]},{'ENS': [20, 182]} , {'ETHFI': [5.35, 200]}]
         self.costPricecountOlStack1 = [{'DOT':[7.06,3925.00]},{'CAKE':[5.13,1147.00]},{'ATOM':[10.00,480.00]},{'ATOM':[9.39,192.59]},{'TIA':[15.00,70.00]},{'MANTA':[1.60,500.00]},{'MANTA':[2.60,20.00]},{'MANTA':[2.93,420.00]},{'MANTA':[2.86,1072.00]},{'MANTA':[2.8,408.00]},{'ZKF':[0.01,94730.00]},{'ZKF':[0.01,72833.00]},{'PYTH':[0.40,6180.00]},{'PYTH':[0.50,80]},{'SOL':[90.00,3.00]},{'ETH':[2300.00,1.04]},{'ETH':[2300.00,1.04]},{'ETH':[2300.00,1.09]},{'BTC ':[41000.00,0.08]}]
+        self.costPricecountsum = [{'BTC': [21328, 6.09]}, {'ETH': [1862, 34]}, {'BNB': [300, 30.3]},
+                                  {'TIA': [8.43, 1786]}, {'WIF': [3.23, 1355]}, {'FET': [2.46, 2383]},
+                                  {'ETHFI': [5.33, 704.6]}, {'DOT': [7.06,4000]},{'SOL': [97.16, 101.98]}, {'ARKM': [2.4, 6622]},
+                                  {'ENS': [16.84, 366]}, {'STX': [2.07, 5970]}, {'FIL': [5.37, 1945]},
+                                  {'UNI': [8.66, 1000]}, {'LINK': [6.82, 550]}, {'NEAR': [1.16, 1187]}, {'APT': [10.12, 590]},
+                                  {'SUI': [1.36, 2158]}, {'PEOPLE': [0.0285, 100164]}, {'DOGE': [0.096, 12040]},
+                                  {'SNX': [3.36, 2001]}, {'DASH': [30, 75.4]}, {'LTC': [85, 30.1]},
+                                  {'SAND': [0.594, 4992]}, {'APE': [1.4, 2181]}, {'GALA': [0.0275, 72783]},
+                                  {'DYDX': [2.84, 1502]}, {'ALT': [0.344, 7605]}, {'ADA': [0.26, 917]},
+                                  {'PEPE': [0.00002, 44688]}, {'ATOM': [10, 700]}, {'LDO': [2.12, 800]}, {'ADA': [1.2, 1318]}, {'LDO': [2.01, 999]}]
 
     def run_get_history_data(self):
         get_history_data(self.symbol_price_history)
@@ -56,14 +66,14 @@ class run_priceX():
     def run_get_current_price(self):
         print("run_get_current_price is running....")
         get_data_price(self.symbol_price, str(datetime.datetime.now().date()), self.today_price_list)
-        #send_ding_msgs("中长线持仓成本价:{}".format(self.costPricedic))
-        send_ding_msgs("日期是:{},百倍币持仓币种数量:{},成本价:{}".format(str(datetime.datetime.now().date()),len(self.baibeisymbol_price), self.baibeisymbol_price,myself='alvin'))
-        send_ding_msgs("日期是:{},百倍币持仓币种数量:{},成本价:{}".format(str(datetime.datetime.now().date()),len(self.baibeisymbol_price), self.baibeisymbol_price),myself='alvin')
+        send_ding_msgs("中长线持仓成本价:{}".format(self.costPricedic))
+        # send_ding_msgs("日期是:{},百倍币持仓币种数量:{},成本价:{}".format(str(datetime.datetime.now().date()),len(self.baibeisymbol_price), self.baibeisymbol_price,myself='alvin'))
+        # send_ding_msgs("日期是:{},百倍币持仓币种数量:{},成本价:{}".format(str(datetime.datetime.now().date()),len(self.baibeisymbol_price), self.baibeisymbol_price),myself='alvin')
 
     def run_get_current_Xprice(self):
         print("run_get_current_Xprice is running....")
         get_data_pricepercentage(self.symbol_price, self.costPricedic, self.today_price_list)
-        X_List = [4,5,6,7,8,9,12,15,20]
+        X_List = [3,4,5,6,7,8,9,10,11,12,15,20]
         for x in X_List:
             get_data_Xprice(self.symbol_price, self.costPricedic, x, self.today_price_list)
         print("run_get_current_Xprice is end !")
@@ -85,9 +95,14 @@ class run_priceX():
             coin_count = 0
 
         print("所有账户汇总持仓品种数量:{},成本U:{},持仓市值U:{},总盈亏U：{},汇总盈亏率:{},浮亏币种数:{},浮亏Token汇总:{}".format(len(list(set(self.sumTotaltokennum))),math_ceil_float(AccountCost3), math_ceil_float(AccountMarketvalue3),math_ceil_float(AccountMarketvalue3-AccountCost3),percentage_allAccount,coin_count,self.sumAccountFloatinglossToken))
-        send_ding_msgs("所有账户汇总持仓品种数量:{},成本U:{},持仓市值U:{},总盈亏U：{},汇总盈亏率:{},浮亏币种数:{},浮亏Token汇总:{}".format(len(list(set(self.sumTotaltokennum))),math_ceil_float(AccountCost3), math_ceil_float(AccountMarketvalue3),math_ceil_float(AccountMarketvalue3-AccountCost3),percentage_allAccount,coin_count,self.sumAccountFloatinglossToken),myself='alvin')
+        # send_ding_msgs("所有账户汇总持仓品种数量:{},成本U:{},持仓市值U:{},总盈亏U：{},汇总盈亏率:{},浮亏币种数:{},浮亏Token汇总:{}".format(len(list(set(self.sumTotaltokennum))),math_ceil_float(AccountCost3), math_ceil_float(AccountMarketvalue3),math_ceil_float(AccountMarketvalue3-AccountCost3),percentage_allAccount,coin_count,self.sumAccountFloatinglossToken),myself='alvin')
         print("run get_current_earnings is end !")
 
+    def run_get_current_earnings_sumall(self):
+        print("run get_current_earnings_sumall is running....")
+        sumTotalAccountCost,sumTotalAccountMarketvalue,AccountFloatingloss = getCostamount(self.costPricecountsum, self.today_price_list ,"sumAll",self.sumTotalAccountCost,self.sumTotalAccountMarketvalue,self.sumTotaltokennum)
+        # 涨幅率=（现价data[1]-原价(costPricedic[symbol]])）/ 原价(costPricedic[symbol]]) * 100%
+        print("run sumTotalAccountCost is {} ,sumTotalAccountMarketvalue is:{} ,AccountFloatingloss is:{}".format(sumTotalAccountCost,sumTotalAccountMarketvalue,AccountFloatingloss))
 
 
 if __name__ == '__main__':
@@ -96,3 +111,4 @@ if __name__ == '__main__':
     run_x.run_get_current_price()
     run_x.run_get_current_Xprice()
     run_x.run_get_current_earnings()
+    run_x.run_get_current_earnings_sumall()
